7468b6434e2ceed184df1f82eb43b7b100efdc03
warriormaster12 <streng.alexander@outlook.com>
2025-01-24 22:47:39 +0200

    Add camera transition

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,7 +8,13 @@ var timer: float = 0.0
 @onready var hud: Control = %HUD
 @onready var timer_label: Label = %TimerLabel
 
+@onready var camera_marker: Marker3D = %CameraPosition
+
 @onready var level_manager: LevelManager = get_owner()
+
+@onready var camera: Camera3D = get_viewport().get_camera_3d()
+
+@onready var world: Node3D = get_child(0)
 
 func _ready() -> void:
 	if !level_manager.main_menu:
@@ -22,8 +28,18 @@ func _process(delta: float) -> void:
 
 
 func start() -> void:
-	timer_on = true
 	hud.visible = true
+	await move_camera()
+	timer_on = true
+
+func move_camera() -> void:
+	camera.reparent(world)
+	var tween: Tween = create_tween().set_parallel()
+	tween.tween_property(camera, "global_position", camera_marker.global_position, 2.0)
+	tween.tween_property(camera, "global_rotation", camera_marker.global_rotation, 2.0)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	
+	await tween.finished
 
 func _update_timer(delta: float) -> void: 
 	if !timer_on: 
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,14 @@
 extends Node
 class_name MainMenu
 
+@export var camera_rotation_speed: float = 0.25
+
 @onready var main_menu_ui: Control = $MainMenuUI
 @onready var start_button: Button = %StartButton
 @onready var instruct_button: Button = %Instructions
 @onready var quit_button: Button = %QuitButton
+
+@onready var camera_rotate: Node3D = $CameraRotate
 
 @onready var level_manager: LevelManager = get_owner()
 
@@ -12,9 +16,16 @@ class_name MainMenu
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_pressed)
 	instruct_button.pressed.connect(_on_instruct_pressed)
-	quit_button.pressed.connect(_on_quit_pressed)
+	
+	if OS.get_name() == "Web":
+		quit_button.queue_free()
+	else:
+		quit_button.pressed.connect(_on_quit_pressed)
 
 
+func _process(delta: float) -> void:
+	camera_rotate.rotation.y -= camera_rotation_speed * delta
+	
 
 func _on_start_pressed() -> void:
 	main_menu_ui.visible = false

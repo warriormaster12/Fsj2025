d11090a2db57ff3a6d79f03374c22e076db50757
Akseli Viitanen <xaryphon@tuta.io>
2025-01-25 11:55:05 +0200

    Add player based bounce direction and destroy on wall

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -8,11 +8,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
-		velocity = -get_gravity() * 0.5
-		velocity.x = (randf() * 2 - 1) * HORIZONTAL_SPEED
-		velocity.z = (randf() * 2 - 1) * HORIZONTAL_SPEED
+		var player_delta: Vector3 = position - %Player.position
+		var direction: Vector3 = player_delta.clamp(Vector3(-1, 0, -1), Vector3(1, 0, 1))
+		direction = 0.75 * direction + 0.25 * Vector3(randf_range(-1, 1), 0, randf_range(-1, 1))
+		velocity = -get_gravity() * 0.5 + 4 * direction
 
-	if position.y < 1:
+	if position.y < 1 || is_on_wall():
 		bubble_destroy.emit()
 		queue_free()
 

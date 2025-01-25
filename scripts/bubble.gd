e9b5f272e6127241848e129be79fb05d20214a7d
Akseli Viitanen <xaryphon@tuta.io>
2025-01-25 21:37:11 +0200

    Fix bubbles bubbles bubbles

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -5,7 +5,7 @@ signal bubble_bounce()
 
 const HORIZONTAL_SPEED = 2
 const GRAVITY: float = 5
-const BOUNCE_STRENGTH_MIN: float = 3
+const BOUNCE_STRENGTH_MIN: float = 5
 const BOUNCE_STRENGTH_MAX: float = 6
 
 var player: CharacterBody3D = null
@@ -24,19 +24,24 @@ func _physics_process(delta: float) -> void:
 	else:
 		bubble_bounce.emit()
 		var player_delta: Vector3 = position - player.position
-		var direction: Vector3 = player_delta.clamp(Vector3(-1, 0, -1), Vector3(1, 0, 1))
+		var direction: Vector3 = player_delta
+		if direction.length_squared() > 1:
+			direction /= direction.length()
 		direction = 0.5 * direction + 1 * Vector3(randf_range(-1, 1), 0, randf_range(-1, 1))
 		velocity = Vector3.UP * randf_range(BOUNCE_STRENGTH_MIN, BOUNCE_STRENGTH_MAX) + 4 * direction
 
+	# Save our velocity since move_and_slide nicely sets it to zero on wall collision
+	var pv: Vector3 = velocity
 	move_and_slide()
 
 	if is_on_wall():
+		velocity = pv
 		var wall: Vector3 = get_wall_normal()
-		if wall.x:
-			velocity.x *= -1
-		if wall.z:
-			velocity.z *= -1
-	
+		if absf(wall.x) > 0.001:
+			velocity.x *= -0.2
+		if absf(wall.z) > 0.001:
+			velocity.z *= -0.2
+
 	var collision: KinematicCollision3D = get_last_slide_collision()
 	if collision && is_on_floor() && !collision.get_collider().is_in_group("player"):
 		bubble_destroy.emit()

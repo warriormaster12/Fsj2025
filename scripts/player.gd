15670f0d825353ea45f985c894b30e349ea6f291
Akseli Viitanen <xaryphon@tuta.io>
2025-01-25 12:24:06 +0200

    Change player acceleration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 10.0
+const SPEED = 16.0
+const ACCELERATION = 24.0
 const FRICTION = 12.0
 
 
@@ -9,8 +10,8 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_l", "move_r", "move_u", "move_d")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction.length_squared() > 0.01:
-		velocity.x = move_toward(velocity.x, direction.x * SPEED, FRICTION * delta)
-		velocity.z = move_toward(velocity.z, direction.z * SPEED, FRICTION * delta)
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION * delta)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		velocity.z = move_toward(velocity.z, 0, FRICTION * delta)

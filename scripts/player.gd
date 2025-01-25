edf56e4453408f142e7d62b7a9b38cfd06675a85
Akseli Viitanen <xaryphon@tuta.io>
2025-01-25 17:52:45 +0200

    Improve ice physics part 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ var PUSM: PowerUpStateManager = null
 
 const SPEED = 16.0
 const ACCELERATION = 24.0
+const SLOWDOWN_ACCELERATION = 6.0
 const FRICTION = 12.0
 const DASH_FORCE = 20.0
 
@@ -12,12 +13,16 @@ func _physics_process(delta: float) -> void:
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	velocity.z = move_toward(velocity.z, 0, FRICTION * delta)
-	if direction.length_squared() > 0.01:
-		var speed_multiplier: float = PUSM.player_speed_multiplier
-		velocity.x = move_toward(velocity.x, speed_multiplier * SPEED * direction.x, speed_multiplier * ACCELERATION * delta)
-		velocity.z = move_toward(velocity.z, speed_multiplier * SPEED * direction.z, speed_multiplier * ACCELERATION * delta)
-		if Input.is_action_just_pressed("dash"):
-			velocity.x = direction.x * DASH_FORCE
-			velocity.z = direction.z * DASH_FORCE
+	var speed_multiplier: float = PUSM.player_speed_multiplier
+	if Input.is_action_just_pressed("dash"):
+		velocity.x = direction.x * DASH_FORCE
+		velocity.z = direction.z * DASH_FORCE
+	else:
+		if abs(direction.x) > 0.001:
+			var acceleration_x: float = ACCELERATION if signf(-velocity.x) != signf(direction.x) else SLOWDOWN_ACCELERATION
+			velocity.x = move_toward(velocity.x, speed_multiplier * SPEED * direction.x, speed_multiplier * acceleration_x * delta)
+		if abs(direction.z) > 0.001:
+			var acceleration_z: float = ACCELERATION if signf(-velocity.z) != signf(direction.z) else SLOWDOWN_ACCELERATION
+			velocity.z = move_toward(velocity.z, speed_multiplier * SPEED * direction.z, speed_multiplier * acceleration_z * delta)
 
 	move_and_slide()

dde319cb5b28e0d51613e13749ca5d488bde21c6
warriormaster12 <streng.alexander@outlook.com>
2025-01-25 19:22:21 +0200

    Cool shader

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -22,8 +22,8 @@ func _physics_process(delta: float) -> void:
 		bubble_bounce.emit()
 		var player_delta: Vector3 = position - player.position
 		var direction: Vector3 = player_delta.clamp(Vector3(-1, 0, -1), Vector3(1, 0, 1))
-		direction = 0.75 * direction + 0.25 * Vector3(randf_range(-1, 1), 0, randf_range(-1, 1))
-		velocity = -get_gravity() * 0.5 + 4 * direction
+		direction = 0.5 * direction + 1 * Vector3(randf_range(-1, 1), 0, randf_range(-1, 1))
+		velocity = -get_gravity() * randf_range(0.75, 1.0) + 4 * direction
 
 	move_and_slide()
 	
--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -5,12 +5,16 @@ class_name LevelManager
 @export var main_menu: MainMenu = null
 @export var game: Game = null
 
+var bubble_count: int = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
 	var found_bubbles:Array[Node] = get_tree().get_nodes_in_group("bubbles")
+	bubble_count = found_bubbles.size()
+	
 	for bubble in found_bubbles:
 		if !bubble.has_signal("bubble_destroy"):
 			continue
@@ -19,5 +23,6 @@ func _process(_delta: float) -> void:
 			bubble.bubble_destroy.connect(_on_bubble_destroyed)
 
 func _on_bubble_destroyed() -> void:
-	if game:
+	bubble_count = clampi(bubble_count -1, 0, bubble_count)
+	if game && bubble_count == 0:
 		game.end()

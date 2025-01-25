0c1fe8cabd078226dd21bfe34e8b5b2a5f4fd435
Akseli Viitanen <xaryphon@tuta.io>
2025-01-25 12:34:53 +0200

    Add dashing

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 const SPEED = 16.0
 const ACCELERATION = 24.0
 const FRICTION = 12.0
-
+const DASH_FORCE = 20.0
 
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_l", "move_r", "move_u", "move_d")
@@ -12,6 +12,9 @@ func _physics_process(delta: float) -> void:
 	if direction.length_squared() > 0.01:
 		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION * delta)
 		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION * delta)
+		if Input.is_action_just_pressed("dash"):
+			velocity.x = direction.x * DASH_FORCE
+			velocity.z = direction.z * DASH_FORCE
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		velocity.z = move_toward(velocity.z, 0, FRICTION * delta)

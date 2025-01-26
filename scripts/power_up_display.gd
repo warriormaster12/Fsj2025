e03aa14a0a5f2dc041d8fbb6d63ae652aa62bd34
Akseli Viitanen <xaryphon@tuta.io>
2025-01-26 13:44:30 +0200

    Add icons to power up display

--- scripts/power_up_display.gd
+++ scripts/power_up_display.gd
@@ -2,8 +2,16 @@ extends HBoxContainer
 class_name PowerUpDisplay
 
 @onready var progress_bar: ProgressBar = %ProgressBar
+@onready var icon: TextureRect = %Icon
 
 var power_up: PowerUpStateManager.PowerUp = null
+
+func _ready() -> void:
+	match power_up.type:
+		PowerUpStateManager.PowerUpType.PLAYER_SPEEDUP:
+			icon.texture = load("res://resources/images/textures/speedboost_static.png")
+		PowerUpStateManager.PowerUpType.TIME_SLOWDOWN:
+			icon.texture = load("res://resources/images/textures/timebendboost_static.png")
 
 func _process(delta: float) -> void:
 	progress_bar.max_value = power_up.initial_duration

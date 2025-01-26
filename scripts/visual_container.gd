613f6a5fd604df55169c8eaa0d375615000acea5
warriormaster12 <streng.alexander@outlook.com>
2025-01-26 12:39:56 +0200

    Add animations

--- scripts/visual_container.gd
+++ scripts/visual_container.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
 @onready var player: PlayerController = get_owner()
-@onready var body: AnimatedSprite3D = $Body
-@onready var eyes: AnimatedSprite3D = $Eyes
+@onready var body_anim_tree: AnimationTree = $BodyAnimationTree
+@onready var eyes_anim_tree: AnimationTree = $EyesAnimTree
+@onready var eyes: Sprite3D = $Eyes
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,17 +11,14 @@ func _ready() -> void:
 
 
 func manage_eyes(direction: Vector3) -> void:
-	pass
+	eyes.flip_h = sign(direction.x) == 1 
+	var running: bool = ceilf(direction.x) == 1.0
+	eyes_anim_tree.set("parameters/idle_run/blend_amount", abs(direction.x))
+	if running:
+		eyes.set("parameters/run_eyes/blend_position", -direction.x if eyes.flip_h else direction.x)
 
 func manage_body(direction: Vector3) -> void:
-	if direction.length() > 0.0:
-		if abs(direction.x) == 1.0:
-			body.play("run")
-		else:
-			body.play("idle")
-		body.flip_h = true if direction.x == 1.0 else false
-	else: 
-		body.play("idle")
+	body_anim_tree.set("parameters/idle_run/blend_position", direction)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:

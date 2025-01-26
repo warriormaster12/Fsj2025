b0ffb251dacc821142ba7880d89835c8ad2abe63
warriormaster12 <streng.alexander@outlook.com>
2025-01-26 12:55:31 +0200

    Add wall textures

--- scripts/visual_container.gd
+++ scripts/visual_container.gd
@@ -8,6 +8,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
 
 
 func manage_eyes(direction: Vector3) -> void:
